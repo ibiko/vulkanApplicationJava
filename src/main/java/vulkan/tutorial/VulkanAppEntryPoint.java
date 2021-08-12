59bced40d682cd0dc2b1ba73d0dc7fac025a9fa9
ibiko <ivan.bikovic.83@gmail.com>
2021-08-12 21:29:57 +0200

    -first checkin

--- src/main/java/vulkan/tutorial/VulkanAppEntryPoint.java
+++ src/main/java/vulkan/tutorial/VulkanAppEntryPoint.java
@@ -3,6 +3,7 @@ package vulkan.tutorial;
 
 public class VulkanAppEntryPoint {
 
+    //firts checkin
     public static void main(String[] args) {
         VulkanApp app = new VulkanApp();
         app.run();
